53abd14bbc8f40a97fd32a039eec5f9e2b2ad8f1
Lester Mesa <lester.mesa@payrix.com>
2020-09-17 08:44:58 -0400

    Added registration controller and routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('register', 'API\RegisterController@register');
+
 Route::middleware('auth:api')->group( function () {
     Route::resource('vehicles', 'API\VehicleController');
 });
